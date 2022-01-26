315ce1f300f7cb163a7edac7bcc393437babea2a
root <root@DESKTOP-QM6Q8NI.localdomain>
2022-01-26 21:31:57 +0700

    uas

---
+++ uas/program2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void data(){
+     string nama;
+     int nim;
+     cout<<"Nama mahasiswa : ";
+     getline(cin,nama);
+     cout<<"NIM mahasiswa : ";
+     cin>>nim;
+}
+int main(){
+    data();
+}
